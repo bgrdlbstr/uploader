12c1f7082536cb30a16fbbc45b50aadcd2bfec4b
bgrdlbstr <bgrdlbstr@users.noreply.github.com>
2021-02-16 20:16:41 +0000

    new puish

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import StyledDropzone from "./StyledDropzone";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <StyledDropzone/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
